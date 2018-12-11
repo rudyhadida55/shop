7179d2d31809de95aaf2689cb1d52ed710e5a6ec
Rudy Hadida <rudy@MacBook-Air-de-Rudy.local>
2018-12-11 16:23:53 +0200

    changing git ignore

--- shop_project/shop_app/models.py
+++ shop_project/shop_app/models.py
@@ -11,3 +11,10 @@ class Product(models.Model):
 
   def __repr__(self):
     return "<Product {}>".format(self.name)
+
+
+class Product(models.Model):
+  name = models.CharField(max_length=264)
+  price = models.DecimalField(max_digits=5, decimal_places=2)
+  description = models.TextField()
+  image = models.ImageField(default='static/images/image.png', upload_to='static/images/')    
